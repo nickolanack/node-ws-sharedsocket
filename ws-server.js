050a383a745776acd62e532f6801304e84b419e1
Nick <nickblackwell@gmail.com>
2015-04-19 20:42:40 -0700

    worked on test class. to figure out an iternaet connectivity issue. kind of unrelated to this project.

--- ws-server.js
+++ ws-server.js
@@ -1,3 +1,18 @@
 /**
  * 
  */
+
+	var wsserver=new (require('ws').Server)({port: 8080});
+	console.log('running: 8089');
+	
+	wsserver.on('connection',function(wsclient){
+		
+		var wsserver=this;
+		console.log('created a new connection');
+		
+		
+	}).on('headers',function(headers){
+		console.log('headers: '+headers);
+	}).on('error',function(error){
+		console.log('error: '+error);
+	});
